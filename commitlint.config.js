269fe092efa0ec7b168c2e47ed31583f18aa96c1
mohan lal sahu <mohansahu18111811@gmailcom>
2024-11-15 16:48:53 +0530

    feature/test

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,16 +1,20 @@
 module.exports = {
-    extends: ["@commitlint/cli", "@commitlint/config-conventional"],
+    extends: ['@commitlint/config-conventional'],
     rules: {
-        "type-enum": [
-            2,
-            "always",
-            [
-                "feature/",
-                "fix/",
-                "refactor/",
-                "build/",
-            ]
-        ],
-        "subject-case": [2, "always", "sentence-case"]
+        'type-enum': [2, 'always', ['feature', 'bug', 'build', 'style', 'refactor']],
+        'type-case': [2, 'always', 'lowercase'],  // Changed from 'lower' to 'lowercase'
+        'subject-case': [0],
+        'type-empty': [2, 'never'],
+        'subject-empty': [2, 'never'],
+        'scope-case': [0],
+        'header-max-length': [2, 'always', 100],
+        'header-full-stop': [2, 'never', '.'],
+        'scope-empty': [0],
+    },
+    parserPreset: {
+        parserOpts: {
+            headerPattern: /^(feature|bug|chore|docs|style|refactor|test|ci)(?:\/([a-z0-9-]+))?\s(.+)$/,
+            headerCorrespondence: ['type', 'scope', 'subject']
+        }
     }
-}
+};
