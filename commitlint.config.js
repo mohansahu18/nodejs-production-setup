06bb4cca5ebfe7459dc17a7881408b24e937a7a0
mohan lal sahu <mohansahu18111811@gmailcom>
2024-11-19 18:04:01 +0530

    feature: lint-fix

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    extends: ["@commitlint/cli", "@commitlint/config-conventional"],
+    extends: ["@commitlint/config-conventional"],
     rules: {
         "type-enum": [
             2,
