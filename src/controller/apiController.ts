6f1d143965c089631170a9326d829aef59c7f529
mohan lal sahu <mohansahu18111811@gmailcom>
2024-11-20 17:35:46 +0530

    feature: health env setup

--- src/controller/apiController.ts
+++ src/controller/apiController.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express'
 import httpResponse from '../util/httpResponse'
 import responseMessage from '../constant/responseMessage'
 import httpError from '../util/httpError'
+import quicker from '../util/quicker'
 
 export default {
     self: (req: Request, res: Response, next: NextFunction) => {
@@ -11,6 +12,19 @@ export default {
             } catch (error) {
                 httpError(next, error, req, 500)
             }
+        }
+    },
+    health: (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const healthData = {
+                application: quicker.getApplicationHealth(),
+                system: quicker.getSystemHealth(),
+                timestamp: Date.now()
+            }
+
+            httpResponse(req, res, 200, responseMessage.SUCCESS, healthData)
+        } catch (err) {
+            httpError(next, err, req, 500)
         }
     }
 }
